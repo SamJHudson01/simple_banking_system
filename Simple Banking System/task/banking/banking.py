2af8af847c090b2a545312c9c41d1c5c766cab15
samhudson <samjhudson@gmail.com>
2021-07-04 15:44:46 +0100

    Stage 3 included

--- Simple Banking System/task/banking/banking.py
+++ Simple Banking System/task/banking/banking.py
@@ -1,10 +1,18 @@
 import random
+import sqlite3
+
+conn = sqlite3.connect('card.s3db')
+cur = conn.cursor()
+cur.execute(
+    "CREATE TABLE IF NOT EXISTS card(id INTEGER , number TEXT, pin TEXT, balance INTEGER DEFAULT 0);")
+conn.commit()
+
 
 
 def start_program():
     print("1. Create an account")
     print("2. Log into account")
-    print("3. Exit")
+    print("0. Exit")
 
 
 class CreditCard:
@@ -23,12 +31,14 @@ class CreditCard:
         if sum(luhn_card) % 10 != 0:
             luhn_digit = 10 - sum(luhn_card) % 10
         self.card_no.append(luhn_digit)
-        self.card_no = ''.join([str(elem) for elem in self.card_no])
-
+        self.card_no = ''.join(str(elem) for elem in self.card_no)
 
         for _ in range(4):
             pin_digit = random.randint(0, 9)
             self.pin += str(pin_digit)
+        cur.execute('INSERT INTO card (number, pin) VALUES (?, ?)',
+                    (self.card_no, self.pin))
+        conn.commit()
         print('Your card has been created')
         print('Your card number:')
         print(self.card_no)
@@ -36,36 +46,37 @@ class CreditCard:
         print(self.pin)
 
 
-all_cards = []
+
 while True:
     start_program()
     customer_selection = int(input())
     if customer_selection == 1:
         new_card = CreditCard()
-        all_cards.append(new_card)
     if customer_selection == 2:
         print('Enter your card number:')
         cust_no = input()
         print('Enter your PIN:')
         cust_pin = input()
-        for card in all_cards:
-            if cust_no == card.card_no and cust_pin == card.pin:
-                print('You have successfully logged in!')
-                print('')
-                print('1. Balance')
-                print('2. Log out')
-                print('0. Exit')
-                cust_selection = int(input())
-                if cust_selection == 1:
-                    print('Balance: 0')
-                elif cust_selection == 2:
-                    print('You have successfully logged out!')
-                    break
-                else:
-                    print('Bye!')
-                    customer_selection = 0
-                    break
+        cur.execute('SELECT number, pin FROM card WHERE number=? AND pin=?', (cust_no, cust_pin,))
+        credentials = cur.fetchone()
+        if credentials:
+            print(credentials)
+            print('You have successfully logged in!')
+            print('')
+            print('1. Balance')
+            print('2. Log out')
+            print('0. Exit')
+            cust_selection = int(input())
+            if cust_selection == 1:
+                print('Balance: 0')
+            elif cust_selection == 2:
+                print('You have successfully logged out!')
+                break
             else:
-                print('Wrong card number or PIN!')
+                print('Bye!')
+                customer_selection = 0
+                break
+        else:
+            print('Wrong card number or PIN!')
     if customer_selection == 0:
         break
